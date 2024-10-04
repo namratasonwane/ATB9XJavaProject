0a43f3826c73d84ea89bc8e279b8775a57b653ee
Dell <namratasonwane18@gmail.com>
2024-10-04 14:07:27 +0530

    Add to GitHub

---
+++ src/Oct/ex_03102024/Lab002_comment.java
@@ -0,0 +1,12 @@
+package Oct.ex_03102024;
+/*
+* This is multi line comments
+* This is used to documentation, reference
+* Author - Pramod
+*
+* */
+public class Lab002_comment {
+    public static void main(String[] args){
+        //System.out.println("Hello"); - This is single line comments
+    }
+}
---
+++ src/TASK_03102024/Task3.java
@@ -0,0 +1,18 @@
+package TASK_03102024;
+
+public class Task3 {
+    public static void main(String[] args){
+        System.out.println("Simple Calculator:");
+        System.out.println("Addition of two number is: ");
+        System.out.println(15+10);
+
+        System.out.println("Substraction of two number is: ");
+        System.out.println(17-5);
+
+        System.out.println("Multiplication of two number is: ");
+        System.out.println(5*15);
+
+        System.out.println("Division of two number is: ");
+        System.out.println(36/4);
+    }
+}
