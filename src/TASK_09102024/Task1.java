a082c9812a6f40d578e151c27b9b27f32a016448
Dell <namratasonwane18@gmail.com>
2024-10-09 17:28:14 +0530

    Add to GitHub

---
+++ src/TASK_09102024/Task1.java
@@ -0,0 +1,27 @@
+package TASK_09102024;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int Num = 10;
+        System.out.printf("Table of %d:\n",Num);
+        System.out.printf("%d * %d = %d",Num,1,Num*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,2,Num*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,3,Num*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,4,Num*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,5,Num*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,6,Num*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,7,Num*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,8,Num*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,9,Num*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d",Num,10,Num*10);
+    }
+}
