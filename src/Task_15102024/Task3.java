96cef8dcff5d13992e94a6f05f9b5197e72da388
Dell <namratasonwane18@gmail.com>
2024-10-16 15:55:54 +0530

    Add to GitHub

--- src/Task_15102024/Task3.java
+++ src/Task_15102024/Task3.java
@@ -3,8 +3,11 @@ package Task_15102024;
 public class Task3 {
     public static void main(String[] args){
         int a = 20;
-
         System.out.println(--a + a++ + a--);
         System.out.println(a);
+
+        // exp 1 -> -- a -> 19 ; a = 19
+        // exp 2 -> a++ -> 19 ; a = 20
+        // exp 3 -> a-- -> 20 ; a = 19
     }
 }
