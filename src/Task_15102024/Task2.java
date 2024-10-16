77912ff324eeb92ec8d1c0df111e15134aba0a80
Dell <namratasonwane18@gmail.com>
2024-10-16 15:55:14 +0530

    Add to GitHub

--- src/Task_15102024/Task2.java
+++ src/Task_15102024/Task2.java
@@ -3,9 +3,12 @@ package Task_15102024;
 public class Task2 {
     public static void main(String[] args){
         int a = 10;
-
         System.out.println(++a + a++ + a++);
-
         System.out.println(a);
+
+        // exp 1 -> ++a -> 11 ; a = 11
+        // exp 2 -> a++ -> 11 ; a = 12
+        // exp 3 -> a++ -> 12 ; a = 13
+        // exp 1 + exp 2 + exp 3 -> 11 + 11 + 12 = 34
     }
 }
