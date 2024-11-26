30956bed247b824691e3e15ddfee9ef303c16d83
Dell <namratasonwane18@gmail.com>
2024-11-26 10:49:41 +0530

    Add to GitHub

---
+++ src/Oct/ex_25102024_string/Lab129_String_immutable_Fix.java
@@ -0,0 +1,11 @@
+package Oct.ex_25102024_string;
+
+public class Lab129_String_immutable_Fix {
+    public static void main(String[] args)
+    {
+        String name = "namrata";
+        name = name.toUpperCase();
+        System.out.println(name);
+
+    }
+}
---
+++ src/Oct/ex_25102024_string/Lab130_String_Equal.java
@@ -0,0 +1,18 @@
+package Oct.ex_25102024_string;
+
+public class Lab130_String_Equal {
+    public static void main(String[] args)
+    {
+        String s1 = "namrata"; //scp
+        String s4 = "namrata"; //scp
+        String s6 = "namrata"; //scp
+
+        String s2 = new String("Hello"); // OA
+        String s3 = new String("Hello"); //OA
+        String s5 = new String("hello");
+
+
+        // == -> comparsion
+        System.out.println();
+    }
+}
